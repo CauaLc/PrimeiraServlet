d2e80cb3a565cb7724894d8d7358abf5ef578c3e
Cauã Luz <125490757+CauaLc@users.noreply.github.com>
2025-04-28 18:09:46 -0300

    Add files via upload

---
+++ ExemploServlets/src/main/java/br/com/fiap/servlets/MinhaPrimeiraServlet.java
@@ -0,0 +1,42 @@
+package br.com.fiap.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class MinhaPrimeiraServlet
+ */
+@WebServlet("/MinhaPrimeiraServlet")
+public class MinhaPrimeiraServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MinhaPrimeiraServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("<b>Olá, Mundo!</b>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
